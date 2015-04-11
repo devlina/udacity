8eac9eb765c11bd80203b7ae62b0f7b36b23602b
Devlina Das <MeNi@home-mbp.local>
2015-04-10 17:12:14 -0700

    Correct formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,7 @@
 import media
 #create a list of all movie instances
-movies = [ media.Movie("FURIOUS 7", "images/fast-furious-7_poster.jpg", "https://www.youtube.com/watch?v=fTo2b9y4Igc"), 
+movies = [ 
+			media.Movie("FURIOUS 7", "images/fast-furious-7_poster.jpg", "https://www.youtube.com/watch?v=fTo2b9y4Igc"), 
 			media.Movie("HOME", "./images/home-poster.jpg", "https://www.youtube.com/watch?v=iLGDJkhYnVc"),
 			media.Movie("Cinderella", "images/cinderella-poster.jpg", "https://www.youtube.com/watch?v=WRuHM6rLSF8"),
 			media.Movie("Lost river", "images/lost river-poster.jpg", "https://www.youtube.com/watch?v=H8ngDiG9V8w"), 
