5d5dddb3f73bea17d7e12423c552519976a040ef
Devlina Das <MeNi@home-mbp.local>
2015-04-10 17:10:44 -0700

    removed external image url

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,6 @@
 import media
 #create a list of all movie instances
-movies = [ media.Movie("FURIOUS 7", "http://cdn.3news.co.nz/3news/AM/2014/10/28/ba438ae3-1bfb-4e0d-bd11-0068d9ed1fe5/fast-furious-7_poster.jpg", "https://www.youtube.com/watch?v=fTo2b9y4Igc"), 
+movies = [ media.Movie("FURIOUS 7", "images/fast-furious-7_poster.jpg", "https://www.youtube.com/watch?v=fTo2b9y4Igc"), 
 			media.Movie("HOME", "./images/home-poster.jpg", "https://www.youtube.com/watch?v=iLGDJkhYnVc"),
 			media.Movie("Cinderella", "images/cinderella-poster.jpg", "https://www.youtube.com/watch?v=WRuHM6rLSF8"),
 			media.Movie("Lost river", "images/lost river-poster.jpg", "https://www.youtube.com/watch?v=H8ngDiG9V8w"), 
